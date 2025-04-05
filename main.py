b3a1d7fbeba757a4d0591aa8bb8258dcf873e780
rtsutsumi153 <rikuttm.12.14@gmail.com>
2025-04-05 12:21:56 +0900

    チャンネルごとにそれぞれファイルを作成し，メッセージ履歴を保存できるよ履歴を保存できるようにした．

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ if __name__ == "__main__":
     # すべてのチャンネルのうち，アプリが追加されているチャンネルのみを抽出して取得
     app_channels = utils.extract_app_channels()
 
-    unique_messages = []
-
     for app_channel in app_channels:
+        unique_messages = []
+        
         messages = utils.get_messages(app_channel["id"], app_channel["name"], users_dict)
         seen_texts = set()  # 出力済みのテキストを記憶するセットを初期化
         for msg in messages:
@@ -29,16 +29,16 @@ if __name__ == "__main__":
             if text not in seen_texts:  #テキストがセットに含まれていなければ出力し，セットに追加する
                 unique_messages.append([app_channel["name"], user, text, thread_ts])
                 seen_texts.add(text)
+        
+        df = pd.DataFrame(unique_messages, columns=["channel_name", "user", "text", "timestamp"])
+        # 保存先ディレクトリを指定
+        save_dir = "conversation_history"  # 例えば "C:/Users/YourName/Documents/output" など
+        os.makedirs(save_dir, exist_ok=True)  # ディレクトリがなければ作成
 
-    df = pd.DataFrame(unique_messages, columns=["channel_name", "user", "text", "timestamp"])
-    # 保存先ディレクトリを指定
-    save_dir = "conversation_history"  # 例えば "C:/Users/YourName/Documents/output" など
-    os.makedirs(save_dir, exist_ok=True)  # ディレクトリがなければ作成
+        # 現在の日付を取得（例：2024-04-03)
+        date_str = datetime.now().strftime("%Y%m%d_%H%M%S")
+        file_name = f"{app_channel["name"]}_{date_str}.csv"
 
-    # 現在の日付を取得（例：2024-04-03)
-    date_str = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_name = f"conversation_history_{date_str}.csv"
-
-    # CSVの保存
-    file_path = os.path.join(save_dir, file_name)
-    df.to_csv(file_path, index=False, encoding='utf-8-sig')
+        # CSVの保存
+        file_path = os.path.join(save_dir, file_name)
+        df.to_csv(file_path, index=False, encoding='utf-8-sig')
