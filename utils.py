b5f81fec821c637adf544d0f88a36a67cc26d503
rtsutsumi153 <rikuttm.12.14@gmail.com>
2025-04-13 15:37:24 +0900

    utils.pyの関数にdoc stringgを記載

--- utils.py
+++ utils.py
@@ -15,7 +15,13 @@ load_dotenv()
 slack_token = os.getenv('SLACK_TOKEN')
 
 def get_response(api_url, payload=None):
-    """slackAPIにリクエストを送ってレスポンスをもらう関数
+    """SlackAPIにリクエストを送ってレスポンスをもらう関数
+    Args:
+        api_url (str): BASE_URL以下に記載する,使用するSlackAPIの関数の名前.
+        payload (dic): リクエストに付加するSlackAPIの関数のパラメータ
+
+    Returns:
+        res.json(): SlackAPIからのレスポンスのjson
     """
     BASE_URL = "https://slack.com/api/"
     headers = {"Authorization": "Bearer {}".format(slack_token)}
@@ -30,6 +36,11 @@ def get_response(api_url, payload=None):
         return res.json()
     
 def get_channels():
+    '''ワークスペースの全てのチャンネルの情報を取得する関数
+    
+    Returns:
+        channels (dic):  ワークスペースの全てのチャンネルの情報
+    '''
     api_url = "conversations.list"
     #payload = {"types": "public_channel,private_channel"}
     channels = get_response(api_url)
@@ -38,6 +49,8 @@ def get_channels():
 
 def extract_app_channels():
     '''すべてのチャンネルのうち，アプリが追加されているチャンネルのみを抽出する関数
+    Returns:
+        app_channels (dic): アプリが追加されているチャンネル
     '''
     # すべてのチャンネルを取得
     channels = get_channels()
@@ -52,6 +65,14 @@ def extract_app_channels():
     
 def get_messages(channel_id, channel_name, users_dict, oldest_ts=0):
     '''指定したチャンネルのメッセージ履歴を取得する関数
+    Args:
+        channel_id (str): チャンネルのID
+        channel_name (str): チャンネルの名前
+        users_dict (dic): ワークスペース内の全てのユーザ情報
+        oldest_ts (str): 前回のメッセージ履歴取得ファイルの一番最後のメッセージのタイムスタンプ
+
+    Returns:
+        messages_sorted: 取得したメッセージ情報をメッセージのタイムスタンプ順でソートしたもの
     '''
     api_url = "conversations.history"
     payload = {
@@ -89,6 +110,12 @@ def get_messages(channel_id, channel_name, users_dict, oldest_ts=0):
 
 def get_oldest_message_ts(save_dir, channel_name):
     '''前回のメッセージ履歴データの最後のメッセージのタイムスタンプを取得
+    Args:
+        save_dir (str): メッセージ履歴データを保存しているディレクトリのパス
+        channel_name (str): メッセージ履歴を取得したいチャンネルの名前
+    
+    Returns:
+        last_row[3]: 前回のメッセージ履歴データの最後のメッセージのタイムスタンプ
     '''
     leatest_file = get_latest_file(save_dir, channel_name) # 最も新しいメッセージ履歴のファイル名を検索
     latest_file_path = f"{save_dir}/{leatest_file}"
@@ -107,6 +134,10 @@ def get_oldest_message_ts(save_dir, channel_name):
 
 def add_csv(save_dir, channel_name, df):
     '''前回のメッセージ取得履歴のファイルに現在のメッセージの差分を追記する関数
+    Args:
+        save_dir (str): メッセージ履歴データを保存しているディレクトリのパス
+        channel_name (str): メッセージ履歴を取得したいチャンネルの名前
+        df (PandasライブラリのDataFrameクラスのインスタンス): 前回のメッセージ取得履歴のファイルからの現在のメッセージの差分
     '''
     # 追記先のファイル（既存のCSV）
     leatest_file = get_latest_file(save_dir, channel_name) # 前回のメッセージ取得履歴のファイル名を検索
@@ -122,6 +153,12 @@ def add_csv(save_dir, channel_name, df):
 
 def get_latest_file(save_dir, channel_name):
     '''これまで記録されているメッセージ履歴の中で最も新しいファイルのファイル名を検索する関数
+    Args:
+        save_dir (str): メッセージ履歴データを保存しているディレクトリのパス
+        channel_name (str): メッセージ履歴を取得したいチャンネルの名前
+    
+    Returns:
+        latest_file: save_dir内のメッセージ履歴データのcsvファイルの中でファイル名に記載されている記録日時が最も新しいもののファイル名
     '''
     # 変数を正規表現に埋め込む（チャンネル名をエスケープして安全に）
     escaped_channel_name = re.escape(channel_name)
@@ -142,6 +179,7 @@ def get_latest_file(save_dir, channel_name):
 
 def remove_first_element(messages):
     '''messagesの先頭の要素を削除して詰める関数
+    
     '''
     if messages:
         return messages[1:]
